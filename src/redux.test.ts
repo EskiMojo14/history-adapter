9ce2a8d2aec0f0e09f9549191aebd45e0401cdbe
EskiMojo14 <ben.j.durrant@gmail.com>
2024-01-07 18:07:08 +0000

    remove desc

--- src/redux.test.ts
+++ src/redux.test.ts
@@ -23,67 +23,65 @@ const book: Book = {
 const newTitle = "The Restaurant at the End of the Universe";
 
 describe("createReduxHistoryAdapter", () => {
-  describe("in combination with RTK", () => {
-    const bookHistorySlice = createSlice({
-      name: "book",
-      initialState: bookHistoryAdapter.getInitialState(book),
-      reducers: (create) => ({
-        undo: create.reducer(bookHistoryAdapter.undo),
-        redo: create.reducer(bookHistoryAdapter.redo),
-        updateTitle: create.preparedReducer(
-          bookHistoryAdapter.withPayload<string>(),
-          bookHistoryAdapter.undoableReducer((state, action) => {
-            state.title = action.payload;
-          }),
-        ),
-        exclaimTitle: create.preparedReducer(
-          bookHistoryAdapter.withoutPayload(),
-          bookHistoryAdapter.undoableReducer((state) => {
-            state.title += "!";
-          }),
-        ),
-      }),
-      selectors: {
-        selectTitle: (state) => state.present.title,
-      },
-    });
+  const bookHistorySlice = createSlice({
+    name: "book",
+    initialState: bookHistoryAdapter.getInitialState(book),
+    reducers: (create) => ({
+      undo: create.reducer(bookHistoryAdapter.undo),
+      redo: create.reducer(bookHistoryAdapter.redo),
+      updateTitle: create.preparedReducer(
+        bookHistoryAdapter.withPayload<string>(),
+        bookHistoryAdapter.undoableReducer((state, action) => {
+          state.title = action.payload;
+        }),
+      ),
+      exclaimTitle: create.preparedReducer(
+        bookHistoryAdapter.withoutPayload(),
+        bookHistoryAdapter.undoableReducer((state) => {
+          state.title += "!";
+        }),
+      ),
+    }),
+    selectors: {
+      selectTitle: (state) => state.present.title,
+    },
+  });
 
-    const { undo, redo, updateTitle } = bookHistorySlice.actions;
-    const { selectTitle } = bookHistorySlice.selectors;
+  const { undo, redo, updateTitle } = bookHistorySlice.actions;
+  const { selectTitle } = bookHistorySlice.selectors;
 
-    const reducer = combineSlices(bookHistorySlice);
-    let store = configureStore({ reducer });
-    beforeEach(() => {
-      store = configureStore({ reducer });
-    });
+  const reducer = combineSlices(bookHistorySlice);
+  let store = configureStore({ reducer });
+  beforeEach(() => {
+    store = configureStore({ reducer });
+  });
 
-    it("can be used as valid case reducers", () => {
-      expect(selectTitle(store.getState())).toBe(book.title);
+  it("can be used as valid case reducers", () => {
+    expect(selectTitle(store.getState())).toBe(book.title);
 
-      store.dispatch(updateTitle(newTitle));
+    store.dispatch(updateTitle(newTitle));
 
-      expect(selectTitle(store.getState())).toBe(newTitle);
+    expect(selectTitle(store.getState())).toBe(newTitle);
 
-      store.dispatch(undo());
+    store.dispatch(undo());
 
-      expect(selectTitle(store.getState())).toBe(book.title);
+    expect(selectTitle(store.getState())).toBe(book.title);
 
-      store.dispatch(redo());
+    store.dispatch(redo());
 
-      expect(selectTitle(store.getState())).toBe(newTitle);
-    });
+    expect(selectTitle(store.getState())).toBe(newTitle);
+  });
 
-    it("can derive undoable from action", () => {
-      expect(selectTitle(store.getState())).toBe(book.title);
+  it("can derive undoable from action", () => {
+    expect(selectTitle(store.getState())).toBe(book.title);
 
-      store.dispatch(updateTitle(newTitle, false));
+    store.dispatch(updateTitle(newTitle, false));
 
-      expect(selectTitle(store.getState())).toBe(newTitle);
+    expect(selectTitle(store.getState())).toBe(newTitle);
 
-      store.dispatch(undo());
+    store.dispatch(undo());
 
-      expect(selectTitle(store.getState())).toBe(newTitle);
-    });
+    expect(selectTitle(store.getState())).toBe(newTitle);
   });
 });
 
