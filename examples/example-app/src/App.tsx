9794fec061bf87c07886526db6c16bcc5c1b16dd
Ben Durrant <ben.j.durrant@gmail.com>
2024-04-27 15:55:49 +0100

    restyle app

--- examples/example-app/src/App.tsx
+++ examples/example-app/src/App.tsx
@@ -1,26 +1,11 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { Count } from "./features/counter/Count";
+import { Count, HistoryButtons } from "./features/counter/Count";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Count />
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Count />
+      <HistoryButtons />
     </>
   );
 }
--- examples/example-app/src/features/counter/Count.tsx
+++ examples/example-app/src/features/counter/Count.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import { useAppDispatch, useAppSelector } from "../../hooks";
 import {
   historyCleared,
@@ -15,33 +16,50 @@ import {
 } from "./counterSlice";
 
 export function Count() {
-  const dispatch = useAppDispatch();
   const count = useAppSelector(selectCount);
+  return <p>count is {count}</p>;
+}
+
+function ButtonGroup({ children }: { children: ReactNode }) {
+  return <div className="button-group">{children}</div>;
+}
+
+export function HistoryButtons() {
+  const dispatch = useAppDispatch();
   const paused = useAppSelector(selectPaused);
   const canUndo = useAppSelector(selectCanUndo);
   const canRedo = useAppSelector(selectCanRedo);
   return (
-    <>
-      <p>count is {count}</p>
-      <button onClick={() => dispatch(incremented(1))}>increment</button>
-      <button onClick={() => dispatch(decremented(1))}>decrement</button>
-      <button onClick={() => dispatch(pauseToggled())}>
-        {paused ? "resume" : "pause"} history
-      </button>
-      <button disabled={!canUndo} onClick={() => dispatch(undone())}>
-        undo
-      </button>
-      <button disabled={!canRedo} onClick={() => dispatch(redone())}>
-        redo
-      </button>
-      <button disabled={!canUndo} onClick={() => dispatch(jumped(-2))}>
-        jump back 2
-      </button>
-      <button disabled={!canRedo} onClick={() => dispatch(jumped(2))}>
-        jump forward 2
-      </button>
-      <button onClick={() => dispatch(historyCleared())}>clear history</button>
+    <div className="card">
+      <ButtonGroup>
+        <button onClick={() => dispatch(incremented(1))}>increment</button>
+        <button onClick={() => dispatch(decremented(1))}>decrement</button>
+      </ButtonGroup>
+      <ButtonGroup>
+        <button disabled={!canUndo} onClick={() => dispatch(undone())}>
+          undo
+        </button>
+        <button disabled={!canRedo} onClick={() => dispatch(redone())}>
+          redo
+        </button>
+      </ButtonGroup>
+      <ButtonGroup>
+        <button disabled={!canUndo} onClick={() => dispatch(jumped(-2))}>
+          jump back 2
+        </button>
+        <button disabled={!canRedo} onClick={() => dispatch(jumped(2))}>
+          jump forward 2
+        </button>
+      </ButtonGroup>
+      <ButtonGroup>
+        <button onClick={() => dispatch(pauseToggled())}>
+          {paused ? "resume" : "pause"} history
+        </button>
+        <button onClick={() => dispatch(historyCleared())}>
+          clear history
+        </button>
+      </ButtonGroup>
       <button onClick={() => dispatch(reset())}>reset</button>
-    </>
+    </div>
   );
 }
