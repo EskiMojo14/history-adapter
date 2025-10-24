cc26232d37105684c0be09eaa663920be51ee76c
Ben Durrant <ben.durrant@marketdojo.com>
2025-10-24 23:09:24 +0100

    update to vitest v4

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,7 @@
-import type { UserConfig } from "vitest/config";
+import type { ViteUserConfig } from "vitest/config";
 import { defineConfig } from "vitest/config";
 
-const config: UserConfig = defineConfig({
+const config: ViteUserConfig = defineConfig({
   test: {
     setupFiles: ["./test-setup"],
   },
